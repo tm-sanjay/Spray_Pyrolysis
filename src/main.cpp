cdc2101d876cf56173984eac63ef2456b5b6cd4b
tmsanjay <tmsanjay.in@gmail.com>
2022-05-25 18:00:17 +0530

    keyoad lib added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <Arduino.h>
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <Keypad.h>
 
-// Set the LCD address to 0x27 for a 16 chars and 2 line display
-LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 /* Features
  * Connect  to the wifi
@@ -30,8 +29,29 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
  * Logs
  * - all run time 
  */
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
 void setup()
-{
+{ 
+  Serial.begin(9600);
+  Serial.println("Spray Pyrolysis System");
 	// initialize the LCD
 	lcd.begin();
 
@@ -44,5 +64,11 @@ void setup()
 
 void loop()
 {
-	
+	char key = keypad.getKey();
+  
+  if (key){
+    lcd.setCursor(0,1);
+    lcd.print(key);
+    Serial.println(key);
+  }
 }
